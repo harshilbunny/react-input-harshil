ab9ec994e69d9ef5141fa471d483a06c8896a43c
Harshil.Mehta <harshil.mehta@coruscate.work>
2020-11-28 16:42:34 +0530

    changes done1.1

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import { ExampleComponent } from 'react-input-harshil'
+import ProgressBar from 'react-input-harshil'
 import 'react-input-harshil/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  const [completed, setCompleted] = useState(0);
 
-export default App
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
+
+  return (
+    <div className="App">
+      <ProgressBar bgcolor={"#6a1b9a"} completed={completed} />
+    </div>
+  );
+};
+
+export default App;
