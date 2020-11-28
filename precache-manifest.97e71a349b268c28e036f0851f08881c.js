62bad148d514e423f8ecb5b319489869f836ef49
Harshil.Mehta <harshil.mehta@coruscate.work>
2020-11-28 17:20:22 +0530

    Updates

---
+++ precache-manifest.97e71a349b268c28e036f0851f08881c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e316fdd63818f4d50c06fd0b7aaa705",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2510c88454e6e0206a36",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "2a7f91192fad59bf5d47",
+    "url": "./static/js/2.dffcfea2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dffcfea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2510c88454e6e0206a36",
+    "url": "./static/js/main.1efcc22d.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.1efcc22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5edbc00d6e21842d65",
+    "url": "./static/js/runtime-main.94a949c9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.383383167e8a30a2bd64e4feeea78e68.js"
+  "./precache-manifest.97e71a349b268c28e036f0851f08881c.js"
 );
 
 self.addEventListener('message', (event) => {
